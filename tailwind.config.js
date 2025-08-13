46ac48993d0daa598de6a8e576901b824f802e37
zeyad-belal <zeyadbelal00@gmail.com>
2025-08-13 21:54:50 +0300

    initial

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./App.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
-
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",        // Expo Router pages/layouts (includes (tabs))
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./constants/**/*.{js,jsx,ts,tsx}",
+  ],
   presets: [require('nativewind/preset')],
   theme: {
     extend: {},
